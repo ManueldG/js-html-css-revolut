678c027f9245d06eb2e8d1a48dc03331129bfe56
Manuel della Gala <manuel.dellagala@gmail.com>
2021-04-10 17:38:18 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 function main(){
     console.log("ok");
+    $('.element').hover(function(){
+        console.log($(this).find('.change'));
+        $(this).find('.change').toggleClass('active');
+        
+    });
+    
+    $('.element').click(function(){
+        $(this).find('.change').toggleClass('activeC');
+        
+    });
 
 }
